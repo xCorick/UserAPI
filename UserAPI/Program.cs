176c868329abb35395c7d8177d6a11464aa2574f
xCorick <corick_serrano12@hotmail.com>
2026-06-17 22:00:21 -0700

    docker

--- UserAPI/Program.cs
+++ UserAPI/Program.cs
@@ -11,6 +11,8 @@ using UserAPI.DataAccess.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
 var jwtSettings = builder.Configuration.GetSection("Jwt");
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
 
 // Add services to the container.
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
